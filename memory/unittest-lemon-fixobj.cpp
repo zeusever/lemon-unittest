f899123a993bf39d34e06f6def3ff32d5d8748f6
yayanyang <yayanyang@gmail.com>
2012-02-15 21:23:24 +0800

    add memory pool unittest
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- memory/unittest-lemon-fixobj.cpp
+++ memory/unittest-lemon-fixobj.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <lemonxx/sys/sys.hpp>
 #include <lemonxx/memory/fixobj.hpp>
+#include <lemonxx/memory/smallobj.hpp>
+#include <lemonxx/memory/pool.hpp>
 #include <lemonxx/unittest/unittest.hpp>
 
 namespace lemon{namespace memory{namespace test{
 
 	struct LemonFixObjectAllocatorUnittest{};
 
-	template<size_t BlockSize,size_t MaxLoops = 100000>
+	template<size_t BlockSize,size_t MaxLoops = 1000000>
 	struct MemoryAllocUnittest
 	{
 	public:
 		void Run()
 		{
+			MemoryPoolTest();
+
+			SmallObjAllocatorTest();
+
 			FixAllocatorTest();
 
 			NewTest();
@@ -20,10 +26,60 @@ namespace lemon{namespace memory{namespace test{
 			MallocFreeTest();
 		}
 	private:
+		void MemoryPoolTest()
+		{
+			pool::allocator<65536> allocator;
+
+			timer_t t;
+
+			for(size_t i = 0 ; i < MaxLoops; ++ i)
+			{
+				_buffer[i] = (byte_t*)allocator.alloc(BlockSize);
+			}
+
+			time_duration d = t.duration();
+
+			std::cout << "pool::allocator alloc " << MaxLoops << " times:" << d << std::endl;
+
+			t.reset();
+
+			allocator.garbagecollect();
+
+			d = t.duration();
+
+			std::cout << "pool::allocator free " << MaxLoops << " times:" << d << std::endl;
+		}
+
+		void SmallObjAllocatorTest()
+		{
+			smallobject::allocator<256> allocator;
+
+			timer_t t;
+
+			for(size_t i = 0 ; i < MaxLoops; ++ i)
+			{
+				_buffer[i] = (byte_t*)allocator.alloc(BlockSize);
+			}
+
+			time_duration d = t.duration();
+
+			std::cout << "smallobject::allocator alloc " << MaxLoops << " times:" << d << std::endl;
+
+			t.reset();
+
+			for(size_t i = 0 ; i < MaxLoops; ++ i)
+			{
+				allocator.free(_buffer[i],BlockSize);
+			}
+
+			d = t.duration();
+
+			std::cout << "smallobject::allocator free " << MaxLoops << " times:" << d << std::endl;
+		}
 
 		void FixAllocatorTest()
 		{
-			fixobject_allocator<BlockSize> allocator;
+			fixed::allocator<BlockSize> allocator;
 
 			timer_t t;
 
@@ -127,6 +183,11 @@ namespace lemon{namespace memory{namespace test{
 	LEMON_UNITTEST_CASE(LemonFixObjectAllocatorUnittest,Byte128AllocTest)
 	{
 		MemoryAllocUnittest<128>().Run();
+	}
+
+	LEMON_UNITTEST_CASE(LemonFixObjectAllocatorUnittest,Byte256AllocTest)
+	{
+		MemoryAllocUnittest<256>().Run();
 	}
 
 }}}
--- memory/unittest-lemon-smallobj.cpp
+++ memory/unittest-lemon-smallobj.cpp
@@ -5,64 +5,6 @@
 
 namespace lemon{namespace memory{namespace test{
 
-	class Object
-	{
-	public:
-		virtual ~Object(){}
-	};
 	
-	class SmallObjUnittest : public Object
-	{
-		char buffer[1];
-	};
-
-	class SmallObject : public smallobj
-	{
-		char buffer[1];
-	};
-
-	LEMON_UNITTEST_CASE(SmallObjUnittest,PerformanceTest)
-	{
-		lemon::timer_t timer;
-
-		static const size_t counter = 1024;
-
-		SmallObjUnittest * objects[counter] = {0};
-
-		SmallObject * smallObjects[counter] = {0};
-
-		timer.reset();
-
-		for(size_t j = 0 ; j < counter ; ++ j)
-		{
-			for(size_t i = 0 ; i < counter ; ++ i)
-			{
-				smallObjects[i] = new SmallObject();
-
-				delete smallObjects[i];
-			}
-		}
-
-		time_duration duration = timer.duration();
-
-		std::cout << "call smallobj new operator:" << duration << std::endl;
-
-		timer.reset();
-
-		for(size_t j = 0 ; j < counter ; ++ j)
-		{
-			for(size_t i = 0 ; i < counter ; ++ i)
-			{
-				objects[i] = new SmallObjUnittest();
-
-				delete objects[i];
-			}
-		}
-		
-
-		duration = timer.duration();
-
-		std::cout << "call c++ buildin new operator:" << duration << std::endl;
-	}
 
 }}}
