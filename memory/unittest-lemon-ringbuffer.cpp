c332681568f141e66f01e8584c4ca7e90861e5d4
yayanyang <yayan.yang@gh-ca.com>
2012-06-04 18:08:43 +0800

    update
    Signed-off-by: yayanyang <yayan.yang@gh-ca.com>

--- memory/unittest-lemon-ringbuffer.cpp
+++ memory/unittest-lemon-ringbuffer.cpp
@@ -6,6 +6,21 @@ namespace lemon{namespace memory{namespace test{
 
 	LEMON_UNITTEST_CASE(RingBufferUnittest,ContructorTest)
 	{
-		ringbuffer::allocator<1024,1> alloc;
+		ringbuffer::allocator<sizeof(size_t),3> alloc;
+
+		size_t capacity = 0;
+
+		while(0 != (capacity = alloc.capacity()))
+		{
+			LEMON_CHECK(alloc.push_front(&capacity,sizeof(size_t)) == 0);
+		}
+
+		void * block  = alloc.push_front(&capacity,sizeof(size_t));
+
+		LEMON_CHECK(0 != block);
+
+		memcpy(&capacity,block,sizeof(capacity));
+
+		LEMON_CHECK(1 == capacity);
 	}
 }}}
