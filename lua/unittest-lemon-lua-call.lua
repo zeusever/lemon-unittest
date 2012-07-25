1fdce40e7f9ba35cea58e1cb22bd9b3d03ac24e3
yayanyang <yayanyang@gmail.com>
2012-07-25 15:51:03 +0800

    update luabind
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- lua/unittest-lemon-lua-call.lua
+++ lua/unittest-lemon-lua-call.lua
@@ -15,3 +15,36 @@ function say_hello(msg,status)
 end
 
 function echo_number(n) return n end
+
+function test(a)
+	print(a)
+	
+	return test
+end
+
+function c_extension_hello()
+	
+	extensions_test.hello()
+	
+	extensions_test.hello2()
+	
+	extensions_test.hello3(type(c_extension_hello))
+	
+	print(extensions_test.hello3("hello world!!!!!!!!!!!"))
+	
+end
+
+
+function cpp_test(t)
+	--for n = 1, counter do
+		t.hello()
+		
+		t.hello2()
+	--end
+end
+
+function cpp_test1()
+	extensions_test.hello()
+end
+
+
