edbe4905dbbaaddf7017dacfc4494187725b1f94
yayanyang <yayanyang@gmail.com>
2012-08-04 16:18:03 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- ppc/unittest-lemon-ppc-codegen.cpp
+++ ppc/unittest-lemon-ppc-codegen.cpp
@@ -7,7 +7,7 @@ namespace lemon{namespace ppc{namespace test{
 
 	LEMON_UNITTEST_CASE(CXXCodeGenUnittest,StdOutTest)
 	{
-		const char * source = "LEMON_PPC_TEST(a,b,c,\"test %1,%2,%3\",1,a.test(),(double)3,4)";
+		const char * source = "LEMON_PPC_TEST(a,b,c,\"test %1,%2,%3\",1,a.test(),(double)3,4)\nLEMON_PPC_TEST(a,b,c,\"test %1,%3\",1,a.test(),4)";
 
 		core::Lexer lexer;
 
@@ -19,9 +19,9 @@ namespace lemon{namespace ppc{namespace test{
 
 		parser.Parse(lexer,ast);
 
-		core::CxxCodeGen codegen;
+		core::CXXCodeGen codegen(lemon::uuid_t(),"LEMON_PPC_TEST","A;B;C",1);
 
-		codegen.Generate("LEMON_PPC_TEST","__lemonppc_unittest_lemon_pcc_codegegen",std::cout,ast);
+		codegen.Generate(std::cout,ast);
 	}
 
 
