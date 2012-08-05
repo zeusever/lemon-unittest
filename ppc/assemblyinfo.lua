81851b1839546b470601f70049174ec69c610e84
yayanyang <yayanyang@gmail.com>
2012-08-05 22:47:36 +0800

    update ppc unittest
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- ppc/assemblyinfo.lua
+++ ppc/assemblyinfo.lua
@@ -2,21 +2,31 @@ assembly =
    {
    -- the project name
    name = "unittest-lemon-ppc";
+
+
    -- the project guid
    guid = "{ 0x612c9e12, 0x2142, 0x4a7d, { 0x8a, 0x11, 0x3c, 0x2e, 0x8e, 0x58, 0xc8, 0x53 } }";
+
+   tracelog = "UNITTEST_LEMON_PPC_TRACE";
+
    -- the project errorcode define
-   tracelog =
+   
+   errorcode=
       {
-		{ 
-			name = "IM_PACKAGE";
-			
-			description = "the Intermedia filter pacakge";
-		};
-		
 		{
-			name = "ENCRYPTO";
-			
-			description = "the Intermedia filter pacakge";
+			name = "OPEN_LUA_ERROR";
+		};
+
+		{
+			name = "LUA_PCALL_ERROR";
+		};
+
+		{
+			name = "LUA_DOFILE_ERROR";
+		};
+
+		{
+			name = "LUA_DUMPLICATE_CLASS_FUNCTION_DEF";
 		};
 	  };
    }
