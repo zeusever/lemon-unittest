7abff9c3bbfc57fd2505a53a7a903bb9e9671e77
yayanyang <yayanyang@gmail.com>
2012-08-30 09:57:03 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

---
+++ trace/assemblyinfo.lua
@@ -0,0 +1,22 @@
+assembly = 
+{
+	name = "unittest-lemon-trace";
+
+	dtrace = 
+	{	
+		catalog = 
+		{
+			{
+				name = "CATALOG_ONE";
+
+				description = "CATALOG_ONE";
+			};
+
+			{
+				name = "CATALOG_TWO";
+
+				description = "CATALOG_TWO";
+			};
+		}; 
+	};
+}
--- trace/unittest-lemon-trace.cpp
+++ trace/unittest-lemon-trace.cpp
@@ -1,6 +1,8 @@
 #include <lemon/trace/abi.h>
 #include <lemonxx/trace/object.hpp>
+#include <lemonxx/function/bind.hpp>
 #include <lemonxx/unittest/unittest.hpp>
+#include <unittest/lemon/trace/assembly.h>
 
 namespace lemon{namespace trace{namespace test{
 
@@ -13,26 +15,56 @@ namespace lemon{namespace trace{namespace test{
 
 	class TraceUnittest{};
 
-	void Trace(const message & msg)
+	void Trace(size_t &counter , size_t loop, condition_variable & cv,lemon::uuid_t id,flag_t flag,lemon_tid_t tid,const message & msg)
 	{
 		description_t description;
 
 		msg.description(description);
 
+		LEMON_CHECK(description.ProcessId == LemonGetProcessID());
 
+		LEMON_CHECK(tid == description.ThreadId);
+
+		LEMON_CHECK(description.Flag == flag);
+
+		LEMON_CHECK(id == *description.Uuid);
+
+		byte_t buffer[256];
+
+		msg.seek(0,lemon::io::seek::begin);
+
+		size_t length = msg.read(buf(buffer));
+
+		LEMON_CHECK(std::string(buffer,buffer + length) == "hello world !!!!");
+
+		++ counter;
+
+		if(counter == loop) cv.notify();
 	}
 
 	LEMON_UNITTEST_CASE(TraceUnittest,LocalTest)
 	{
 		lemon::uuid_t id;
 
+		condition_variable cv;
+
+		mutex_t	mutex;
+
+		flag_t flag = LEMON_MAKE_TRACE_FLAG(LEMON_TL_DEBUG,UNITTEST_LEMON_TRACE_CATALOG_ONE);
+
+		flag_t allflag = LEMON_MAKE_TRACE_FLAG(LEMON_TL_ANY,UNITTEST_LEMON_TRACE_CATALOG_ONE | UNITTEST_LEMON_TRACE_CATALOG_TWO);
+
 		service s;
+
+		size_t counter = 0;
+
+		size_t loop = 10000;
 
 		provider p(s,&id);
 
 		controller c(s);
 
-		conumser cs(c,&Trace);
+		conumser cs(c,lemon::bind(&Trace,lemon::ref(counter),loop,lemon::ref(cv),id,flag,current_thread_id(),lemon::_0));
 
 		LEMON_CHECK(!cs.empty());
 
@@ -40,9 +72,33 @@ namespace lemon{namespace trace{namespace test{
 
 		LEMON_CHECK(cs1.empty());
 
-		cs1.start(c,&Trace);
+		cs1.start(c,lemon::bind(&Trace,lemon::ref(counter),loop,lemon::ref(cv),id,flag,current_thread_id(),lemon::_0));
 
 		LEMON_CHECK(!cs1.empty());
+
+		cs1.close();
+
+		LEMON_CHECK(cs1.empty());
+
+		c.open_trace(&id,allflag);
+
+		mutex_t::scope_lock lock(mutex);
+
+		for(size_t i = 0 ; i < loop; ++ i)
+		{
+			message_commiter cm(p,flag);
+
+			LEMON_CHECK(!cm.empty());
+
+			cm.write(cbuf("hello world !!!!"));
+
+			cm.commit();
+
+			LEMON_CHECK(cm.empty());
+		}
+
+		cv.wait(lock);
+		
 	}
 
 }}}
