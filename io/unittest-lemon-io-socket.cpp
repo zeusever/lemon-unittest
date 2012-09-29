96d185f3e94d2f7eba9bf5b68084dc39b26d0e7f
yayanyang <yayanyang@gmail.com>
2012-09-29 21:47:41 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

---
+++ io/unittest-lemon-io-socket.cpp
@@ -0,0 +1,44 @@
+#include <lemonxx/io/tcp.hpp>
+#include <lemonxx/function/bind.hpp>
+#include <lemonxx/unittest/unittest.hpp>
+
+namespace lemon{namespace io{namespace test{
+	
+	class SocketUnittest{};
+
+	void SyncServer(net::endpoint ep,io_service & service)
+	{
+		tcp::server server(ep,service);
+
+		server.listen(SOMAXCONN);
+
+		net::endpoint remote;
+
+		tcp::connection cnn(server.accept(remote));
+
+		cnn.send(lemon::cbuf("hello world"));
+	}
+
+	LEMON_UNITTEST_CASE(SocketUnittest,SyncAPITest)
+	{
+		io_service service;
+
+		net::resolver_iterator iter("127.0.0.1","18120");
+
+		tcp::client client(iter->ai_family,service);
+
+		net::endpoint remote(iter->ai_addr);
+
+		lemon::thread_t server(lemon::bind(&SyncServer,remote,ref(service)));
+
+		sleep(4000);
+
+		client.connect(remote);
+
+		char buffer[255] = {0};
+
+		client.receive(lemon::buf(buffer));
+
+		server.join();
+	}
+}}}
