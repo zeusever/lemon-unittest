2c7adf578badc23d6ad8f61ee2ef67a22d4e8365
yayanyang <yayanyang@gmail.com>
2012-11-05 18:25:45 +0800

    update
    Signed-off-by: yayanyang <yayanyang@gmail.com>

--- io/unittest-lemon-io-irp.cpp
+++ io/unittest-lemon-io-irp.cpp
@@ -1,4 +1,6 @@
+#include <hash_map>
 #include <lemon/io/irp.h>
+#include <lemonxx/sys/sys.hpp>
 #include <lemonxx/unittest/unittest.hpp>
 
 namespace lemon{namespace io{namespace test{
@@ -15,7 +17,7 @@ namespace lemon{namespace io{namespace test{
 
 		LEMON_CHECK(LEMON_CHECK_HANDLE(table));
 
-		LemonIRPTableFileObj obj  = LemonCreateIRPTableFileObj(table,NULL,(__lemon_io_file)100,errorCode);
+		LemonIRPTableFileObj obj  = LemonCreateIRPTableFileObj(table,(__lemon_io_file)100,errorCode);
 
 		LEMON_CHECK(LEMON_SUCCESS(errorCode));
 
@@ -79,7 +81,7 @@ namespace lemon{namespace io{namespace test{
 
 		LEMON_CHECK(LEMON_CHECK_HANDLE(table));
 
-		LemonIRPTableFileObj obj  = LemonCreateIRPTableFileObj(table,NULL,(__lemon_io_file)100,errorCode);
+		LemonIRPTableFileObj obj  = LemonCreateIRPTableFileObj(table,(__lemon_io_file)100,errorCode);
 
 		LEMON_CHECK(LEMON_SUCCESS(errorCode));
 
@@ -109,4 +111,145 @@ namespace lemon{namespace io{namespace test{
 		LemonCloseIRPTable_TS(table);
 	}
 
+	class LemonIRPTableUnittest{};
+
+	LEMON_UNITTEST_CASE(LemonIRPTableUnittest,LemonIRPTableFileObjTest)
+	{
+		lemon::error_info errorCode;
+
+		LemonIRPTable table = LemonCreateIRPTable_TS(errorCode);
+
+		LEMON_CHECK(LEMON_SUCCESS(errorCode));
+
+		LEMON_CHECK(LEMON_CHECK_HANDLE(table));
+
+		size_t counter = 10000;
+
+		for(size_t i = 0; i < counter; ++i)
+		{
+			LemonIRPTableFileObj obj  = LemonCreateIRPTableFileObj(table,(__lemon_io_file)i,errorCode);
+
+			LemonIRPTableAdd(table,obj,errorCode);
+
+			LEMON_CHECK(LEMON_SUCCESS(errorCode));
+		}
+
+		lemon::random_t random;
+
+		for(size_t i = 0; i < counter; ++i)
+		{
+			LEMON_CHECK(LEMON_CHECK_HANDLE(LemonIRPTableSearch(table,(__lemon_io_file)(random.next() % counter))));
+		}
+		LEMON_CHECK(LemonIRPTableSize(table) == counter);
+
+		LemonIRPTableFileObj obj = LemonIRPTableSearch(table,(__lemon_io_file)655);
+
+		LEMON_CHECK(LEMON_CHECK_HANDLE(obj));
+
+		LemonIRPTableRemove(table,obj);
+
+		LEMON_CHECK(LemonIRPTableSize(table) == counter - 1);
+
+		LEMON_CHECK(LemonIRPTableLoadFactor(table) <= LEMON_IO_HASHMAP_LOAD_FACTOR);
+
+		LemonCloseIRPTable_TS(table);
+	}
+
+	LEMON_UNITTEST_CASE(LemonIRPTableUnittest,HashFunctionTest)
+	{
+		for(size_t i = 0; i < 100000000; ++ i)
+		{
+			LemonIRPTableHashF(1024,(__lemon_io_file)i);
+		}
+	}
+
+	LEMON_UNITTEST_CASE(LemonIRPTableUnittest,TableInsertRemoveTest)
+	{
+		lemon::error_info errorCode;
+
+		LemonIRPTable table = LemonCreateIRPTable_TS(errorCode);
+
+		LEMON_CHECK(LEMON_SUCCESS(errorCode));
+
+		LEMON_CHECK(LEMON_CHECK_HANDLE(table));
+
+		size_t counter = 1000000;
+
+		for(size_t i = 0; i < counter; ++i)
+		{
+			LemonIRPTableFileObj obj  = LemonCreateIRPTableFileObj(table,(__lemon_io_file)i,errorCode);
+
+			LemonIRPTableAdd(table,obj,errorCode);
+
+			LEMON_CHECK(LEMON_SUCCESS(errorCode));
+		}
+
+		for(size_t i = 0; i < counter; ++i)
+		{
+			LemonIRP irp = LemonCreateIRP_TS(table,errorCode);
+
+			LemonInsertIRP_TS(table,(__lemon_io_file)623,irp,errorCode);
+		}
+
+		LEMON_CHECK(LemonIRPCounter_TS(table,(__lemon_io_file)623) == counter);
+
+		LEMON_CHECK(LemonIRPCounter_TS(table,(__lemon_io_file)(counter + 10)) == 0);
+
+		LemonRemoveIRPsAll_TS(table,(__lemon_io_file)623);
+
+		LEMON_CHECK(LemonIRPCounter_TS(table,(__lemon_io_file)623) == 0);
+
+		LemonCloseIRPTable_TS(table);
+	}
+
+	LEMON_UNITTEST_CASE(LemonIRPTableUnittest,TableInsertPerformanceTest)
+	{
+		lemon::error_info errorCode;
+
+		LemonIRPTable table = LemonCreateIRPTable_TS(errorCode);
+
+		LEMON_CHECK(LEMON_SUCCESS(errorCode));
+
+		LEMON_CHECK(LEMON_CHECK_HANDLE(table));
+
+		size_t counter = 2000000;
+
+		for(size_t i = 0; i < counter; ++i)
+		{
+			LemonIRP irp = LemonCreateIRP_TS(table,errorCode);
+
+			LemonInsertIRP_TS(table,(__lemon_io_file)i,irp,errorCode);
+		}
+
+		LemonCloseIRPTable_TS(table);
+	}
+
+	LEMON_UNITTEST_CASE(LemonIRPTableUnittest,STDHashTablePerformanceTest)
+	{
+		std::hash_map<size_t,int> hashTable;
+
+		size_t counter = 10000;
+
+		for(size_t i = 0; i < counter; ++i)
+		{
+			hashTable.insert(std::make_pair(i,1));
+		}
+
+		lemon::random_t random;
+
+		lemon::timer_t clock;
+
+		for(size_t i = 0; i < counter; ++i)
+		{
+			hashTable[random.next() % counter];
+		}
+
+		time_duration duration = clock.duration();
+
+		std::cout << "search (" << duration / 10000000 << "." 
+
+			<< std::setw(6) << std::setfill('0') <<(duration % 10000000) / 10 
+
+			<< " s)" << std::endl;
+	}
 }}}
